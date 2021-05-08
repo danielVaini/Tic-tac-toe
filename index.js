7a52b7331e59f31f0b11a42ca0579f57d323cc6d
Daniel Vaini <dnlvaini@gmail.com>
2021-05-08 08:30:39 -0300

    feat: Creating PC moves

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ let cont = 0;
 // a tie
 let end = document.querySelector('.end');
 let userWinner = document.querySelector('.winner')
+// Button reset game
+const resetButton = document.querySelector('.reset')
+
+
 
 let array = [
   [0,0,0],
@@ -26,7 +30,6 @@ function refresScoreBoard(){
 
 buttons.forEach(item => {
   item.addEventListener('click', () => {
-    console.log(winner(array))
     if(cont % 2 == 0){
       item.innerHTML = "X";
       item.disabled = true;
@@ -61,34 +64,34 @@ buttons.forEach(item => {
       
       }
       cont++
+      
       winner(array);
-  
+      
     }else {
-     
       item.innerHTML = "O"
       item.disabled = true;
       switch (item.className[0]) {
         case '1':
           array[0][0] = 'o'
           break;
-        case '2':
-          array[0][1] = 'o'
-          break;
-        case '3':
-          array[0][2] = 'o'
-          break;
-        case '4':
-          array[1][0] = 'o'
-          break;
-        case '5':
-          array[1][1] = 'o'
-          break;
-        case '6':
-          array[1][2] = 'o'
-          break;
-        case '7':
-          array[2][0] = 'o'
-          break;
+          case '2':
+            array[0][1] = 'o'
+            break;
+            case '3':
+              array[0][2] = 'o'
+              break;
+              case '4':
+                array[1][0] = 'o'
+                break;
+                case '5':
+                  array[1][1] = 'o'
+                  break;
+                  case '6':
+                    array[1][2] = 'o'
+                    break;
+                    case '7':
+                      array[2][0] = 'o'
+                      break;
         case '8':
           array[2][1] = 'o'
           break;
@@ -98,6 +101,7 @@ buttons.forEach(item => {
       
       }
       cont++
+    
       winner(array);
      
     }
@@ -107,7 +111,11 @@ buttons.forEach(item => {
 
 function verify(cont) {
   if(cont == 9) {
+    countParty++;
+    resetButton.innerHTML = "New Game";
+    refresScoreBoard();
     end.innerHTML = 'Empate'
+    
   }
 }
 
@@ -122,183 +130,202 @@ function winner(array) {
     })
     countXWin++;
     countParty++;
+    resetButton.innerHTML = "New Game";
     refresScoreBoard()
     return true;
     
   }
   // X winner  line 2 -> <- 
-  if(array[1][0] == 'x' && array[1][1] == 'x' && array[1][2] == 'x'){
+  else if(array[1][0] == 'x' && array[1][1] == 'x' && array[1][2] == 'x'){
     userWinner.innerHTML = "X venceu"
     buttons.forEach(item => {
       item.disabled = true;
     })
    countXWin++;
    countParty++;
+   resetButton.innerHTML = "New Game";
    refresScoreBoard()
    return true;
   }
   // X winner  line 3 -> <- 
-  if(array[2][0] == 'x' && array[2][1] == 'x' && array[2][2] == 'x'){
+  else if(array[2][0] == 'x' && array[2][1] == 'x' && array[2][2] == 'x'){
     userWinner.innerHTML = "X venceu"
     buttons.forEach(item => {
       item.disabled = true;
     })
    countXWin++;
    countParty++;
+   resetButton.innerHTML = "New Game";
    refresScoreBoard()
    return true;
   }
   // X winner column 1
-  if(array[0][0] == 'x' && array[1][0] == 'x' && array[2][0] == 'x'){
+  else if(array[0][0] == 'x' && array[1][0] == 'x' && array[2][0] == 'x'){
     userWinner.innerHTML = "X venceu"
     buttons.forEach(item => {
       item.disabled = true;
     })
    countXWin++;
    countParty++;
+   resetButton.innerHTML = "New Game";
    refresScoreBoard()
    return true;
   }
   // X winner column 2
-  if(array[0][1] == 'x' && array[1][1] == 'x' && array[2][1] == 'x'){
+  else if(array[0][1] == 'x' && array[1][1] == 'x' && array[2][1] == 'x'){
     userWinner.innerHTML = "X venceu"
     buttons.forEach(item => {
       item.disabled = true;
     })
    countXWin++;
    countParty++;
+   resetButton.innerHTML = "New Game";
    refresScoreBoard()
    return true;
   }
   // X winner column 3
-  if(array[0][2] == 'x' && array[1][2] == 'x' && array[2][2] == 'x'){
+  else if(array[0][2] == 'x' && array[1][2] == 'x' && array[2][2] == 'x'){
     userWinner.innerHTML = "X venceu"
     buttons.forEach(item => {
       item.disabled = true;
     })
    countXWin++;
    countParty++;
+   resetButton.innerHTML = "New Game";
    refresScoreBoard()
    return true;
   }
   // X winner \
-  if(array[0][0] == 'x' && array[1][1] == 'x' && array[2][2] == 'x'){
+  else if(array[0][0] == 'x' && array[1][1] == 'x' && array[2][2] == 'x'){
     userWinner.innerHTML = "X venceu"
     buttons.forEach(item => {
       item.disabled = true;
     })
    countXWin++;
    countParty++;
+   resetButton.innerHTML = "New Game";
    refresScoreBoard()
    return true;
   }
   // X winner /
-  if(array[0][2] == 'x' && array[1][1] == 'x' && array[2][0] == 'x'){
+  else if(array[0][2] == 'x' && array[1][1] == 'x' && array[2][0] == 'x'){
     userWinner.innerHTML = "X venceu"
     buttons.forEach(item => {
       item.disabled = true;
     })
    countXWin++;
    countParty++;
+   resetButton.innerHTML = "New Game";
    refresScoreBoard()
    return true;
   }
 
    // O winner  line 1 -> <- 
-   if(array[0][0] == 'o' && array[0][1] == 'o' && array[0][2] == 'o'){
+   else if(array[0][0] == 'o' && array[0][1] == 'o' && array[0][2] == 'o'){
     userWinner.innerHTML = "O venceu"
     buttons.forEach(item => {
       item.disabled = true;
     })
     countOWin++;
     countParty++;
+    resetButton.innerHTML = "New Game";
     refresScoreBoard()
     return true;
   }
   // O winner  line 2 -> <- 
-  if(array[1][0] == 'o' && array[1][1] == 'o' && array[1][2] == 'o'){
+  else if(array[1][0] == 'o' && array[1][1] == 'o' && array[1][2] == 'o'){
     userWinner.innerHTML = "O venceu"
     buttons.forEach(item => {
       item.disabled = true;
     })
     countOWin++;
     countParty++;
+    resetButton.innerHTML = "New Game";
     refresScoreBoard()
     return true;
   }
   // o winner  line 3 -> <- 
-  if(array[2][0] == 'o' && array[2][1] == 'o' && array[2][2] == 'o'){
+  else if(array[2][0] == 'o' && array[2][1] == 'o' && array[2][2] == 'o'){
     userWinner.innerHTML = "O venceu"
     buttons.forEach(item => {
       item.disabled = true;
     })
     countOWin++;
     countParty++;
+    resetButton.innerHTML = "New Game";
     refresScoreBoard()
     return true;
   }
   // o winner column 1
-  if(array[0][0] == 'o' && array[1][0] == 'o' && array[2][0] == 'o'){
+  else if(array[0][0] == 'o' && array[1][0] == 'o' && array[2][0] == 'o'){
     userWinner.innerHTML = "O venceu"
     buttons.forEach(item => {
       item.disabled = true;
     })
     countOWin++;
     countParty++;
+    resetButton.innerHTML = "New Game";
     refresScoreBoard()
     return true;
   }
   // o winner column 2
-  if(array[0][1] == 'o' && array[1][1] == 'o' && array[2][1] == 'o'){
+  else if(array[0][1] == 'o' && array[1][1] == 'o' && array[2][1] == 'o'){
     userWinner.innerHTML = "O venceu"
     buttons.forEach(item => {
       item.disabled = true;
     })
     countOWin++;
     countParty++;
+    resetButton.innerHTML = "New Game";
     refresScoreBoard()
     return true;
   }
   // o winner column 3
-  if(array[0][2] == 'o' && array[1][2] == 'o' && array[2][2] == 'o'){
+  else if(array[0][2] == 'o' && array[1][2] == 'o' && array[2][2] == 'o'){
     userWinner.innerHTML = "O venceu"
     buttons.forEach(item => {
       item.disabled = true;
     })
     countOWin++;
     countParty++;
+    resetButton.innerHTML = "New Game";
     refresScoreBoard()
     return true;
   }
   // o winner \
-  if(array[0][0] == 'o' && array[1][1] == 'o' && array[2][2] == 'o'){
+  else if(array[0][0] == 'o' && array[1][1] == 'o' && array[2][2] == 'o'){
     userWinner.innerHTML = "O venceu"
     buttons.forEach(item => {
       item.disabled = true;
     })
     countOWin++;
     countParty++;
+    resetButton.innerHTML = "New Game";
     refresScoreBoard()
     return true;
   }
   // o winner /
-  if(array[0][2] == 'o' && array[1][1] == 'o' && array[2][0] == 'o'){
+  else if(array[0][2] == 'o' && array[1][1] == 'o' && array[2][0] == 'o'){
     userWinner.innerHTML = "O venceu"
     buttons.forEach(item => {
       item.disabled = true;
     })
     countOWin++;
     countParty++;
+    resetButton.innerHTML = "New Game";
     refresScoreBoard()
     return true;
+  }else {
+    return verify(cont);
   }
-  return false
+ 
 }
 
-const resetButton = document.querySelector('.reset')
+
 
 resetButton.addEventListener('click', () => {
   reset()
+  console.log(cont)
 })
 
 function reset(){
@@ -307,15 +334,20 @@ function reset(){
     [0,0,0],
     [0,0,0]
   ]
-
+  
   buttons.forEach(item => {
     item.innerHTML = ""
     item.disabled = false
   })
 
   userWinner.innerHTML = "-"
+  cont = 0;
+  end.innerHTML = "";
+  resetButton.innerHTML = "Reset Game"
 }
 
-function pcTurn(array) {
-  
+function pcTurn(array, level) {
+  if(level == "easy"){
+    let random = Math.round(Math.random() * 9)
+  }
 }
